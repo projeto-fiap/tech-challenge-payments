79a15cbbfa3d6b50134fd5c455bfb75b4728bf35
GVRO2 <141280423+GVRO2@users.noreply.github.com>
2025-01-13 20:12:45 -0300

    add testes

--- src/test/java/tech/fiap/project/infra/configuration/authorization/WebSecurityConfigTest.java
+++ src/test/java/tech/fiap/project/infra/configuration/authorization/WebSecurityConfigTest.java
@@ -1,25 +1,62 @@
 package tech.fiap.project.infra.configuration.authorization;
 
-import org.junit.jupiter.api.BeforeEach;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.security.config.annotation.ObjectPostProcessor;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
 
-@Import(WebSecurityConfig.class)
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+
+@Import({WebSecurityConfig.class})
 @EnableWebSecurity
 class WebSecurityConfigTest {
 
-	private KeycloakJwtAuthenticationConverter keycloakJwtAuthenticationConverter;
+	private final KeycloakJwtAuthenticationConverter keycloakJwtAuthenticationConverter = new KeycloakJwtAuthenticationConverter(new ObjectMapper());
 
-	@BeforeEach
-	void setUp() {
-		keycloakJwtAuthenticationConverter = Mockito.mock(KeycloakJwtAuthenticationConverter.class);
+	ObjectPostProcessor<Object> objectPostProcessor = new ObjectPostProcessor<Object>() {
+		@Override
+		public <O> O postProcess(O object) {
+			return object;
+		}
+	};
+	private HttpSecurity httpSecurity;	{
+		HashMap<Class<?>,Object> sharedObjects = new HashMap<>();
+		GenericApplicationContext value = new GenericApplicationContext();
+		value.getBeanFactory().registerSingleton("jwtDecoder", NimbusJwtDecoder.withJwkSetUri("https://your-auth-server/.well-known/jwks.json").build());
+		value.getBeanFactory().registerSingleton("clientRegistrationRepository'", new ClientRegistrationRepository() {
+			@Override
+			public ClientRegistration findByRegistrationId(String registrationId) {
+				return null;
+			}
+		});
+		value.refresh();
+		sharedObjects.put(ApplicationContext.class, value);
+		httpSecurity = Mockito.spy(new HttpSecurity(objectPostProcessor, new AuthenticationManagerBuilder(objectPostProcessor), sharedObjects));
 	}
 
+	private WebSecurityConfig webSecurityConfig = new WebSecurityConfig(keycloakJwtAuthenticationConverter);
+
 	@Test
-	void testJwtAuthenticationConverter_shouldBeCalled() {
-		Mockito.verify(keycloakJwtAuthenticationConverter, Mockito.never()).convert(Mockito.any());
+	void filterChain_shouldConfigureHttpSecurity() throws Exception {
+		SecurityFilterChain securityFilterChain = webSecurityConfig.filterChain(httpSecurity);
+		assertNotNull(securityFilterChain);
+		verify(httpSecurity).authorizeHttpRequests(any());
+		verify(httpSecurity).oauth2Client(any());
+		verify(httpSecurity).oauth2ResourceServer(any());
 	}
 
 }

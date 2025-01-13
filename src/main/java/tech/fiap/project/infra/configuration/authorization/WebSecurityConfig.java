be842ecd31936098f463fb7aa6cbc0106273286f
GVRO2 <141280423+GVRO2@users.noreply.github.com>
2025-01-13 00:06:39 -0300

    fix sonar quality

--- src/main/java/tech/fiap/project/infra/configuration/authorization/WebSecurityConfig.java
+++ src/main/java/tech/fiap/project/infra/configuration/authorization/WebSecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -21,8 +20,7 @@ public class WebSecurityConfig {
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
-				.csrf(AbstractHttpConfigurer::disable).oauth2Client(withDefaults()).oauth2ResourceServer(
+		http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated()).oauth2Client(withDefaults()).oauth2ResourceServer(
 						httpSecurityOAuth2ResourceServerConfigurer -> httpSecurityOAuth2ResourceServerConfigurer
 								.jwt(jwtConfigurer -> jwtConfigurer
 										.jwtAuthenticationConverter(keycloakJwtAuthenticationConverter)));

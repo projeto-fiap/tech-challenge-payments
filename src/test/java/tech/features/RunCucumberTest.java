4e30ccf80b6fc1eb8c596b3ee39a08ff7a7587d6
Matheus <matheuspatricio999@gmail.com>
2025-02-15 17:27:10 -0300

    adicionando bdd e testes integrados

---
+++ src/test/java/tech/features/RunCucumberTest.java
@@ -0,0 +1,12 @@
+package tech.features;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/features", glue = "tech.features",
+		plugin = { "pretty", "html:target/cucumber-report.html" })
+public class RunCucumberTest {
+
+}
